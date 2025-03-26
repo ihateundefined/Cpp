d5c0c4ab2dec3da7a0dfc45d275bfd2c3eed7ef0
Sunhee Cho <ihateundefined@gamil.com>
2025-03-26 14:15:07 +0900

    dailytask Developer

--- 250326_dailyTask1.cpp
+++ 250326_dailyTask1.cpp
@@ -12,6 +12,67 @@ Display()를 오버라이딩 한 후 출력하라.
 
 using namespace std;
 
+class Employee
+{
+public:
+	Employee();
+	Employee(const char* szName, const char* szAddr);
+	~Employee();
+	void Display();
+protected:
+	char* pName;
+	char* pAddr;
+};
+
+Employee::Employee()
+{
+	pName = NULL;
+	pAddr = NULL;
+}
+
+Employee::Employee(const char* strName, const char* strAddr)
+{
+	cout << "인자가 2개인 생성자 호출" << endl;
+	pName = new char[strlen(strName) + 1];
+	pAddr = new char[strlen(strAddr) + 1];
+	strcpy_s(pName, strlen(strName) + 1, strName);
+	strcpy_s(pAddr, strlen(strAddr) + 1, strAddr);
+}
+
+Employee::~Employee()
+{
+	delete[] pName;
+	delete[] pAddr;
+}
+
+void Employee::Display()
+{
+	cout << "이름 :" << pName << endl;
+	cout << "주소 :" << pAddr << endl;
+}
+
+class Manager : public Employee {
+private:
+	char* pRole;
+public:
+	Manager();
+	~Manager()
+	{
+		delete[] pRole;
+	};
+	Manager(const char* pName, const char* pAddr, const char* strRole) : Employee(pName, pAddr)
+	{
+		pRole = new char[strlen(strRole) + 1];
+		strcpy_s(pRole, strlen(strRole) + 1, strRole);
+	}
+	void Display()
+	{
+		cout << "이름 :" << pName << endl;
+		cout << "주소 :" << pAddr << endl;
+		cout << "역할 : " << pRole << endl;
+	}
+};
+
 void main()
 {
 
